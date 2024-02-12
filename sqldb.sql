0c38f9cb76f5efb1dc067e0bf66d16a0c27ef067
Hamza Shafi <37909988+Hamzashafi@users.noreply.github.com>
2024-02-12 13:15:34 +0500

    SQL workbench
    Commit new files and data

---
+++ sqldb.sql
@@ -0,0 +1,100 @@
+alter table employees
+rename column phone_number to email;
+
+select * from employees;
+
+alter table employees
+modify column email varchar(100);
+
+select * from employees;
+
+alter table employees
+modify email varchar(100)
+after last_name;
+
+select * from employees;
+
+alter table employees
+drop column email;
+
+insert into employees
+values (
+1, 
+"hamza",
+"shafi", 
+25.45, 
+"2024-02-01"
+);
+
+insert into employees
+values (2, "usman", "khan", 20.24, "2024-03-20"),
+	   (3, "akram", "saleem", 15.20, "2023-05-15"),
+       (4, "saif", "aslam", 21.43, "2023-03-15"),
+       (5, "shani", "wajahat", 13.45, "2023-09-12");
+       
+insert into employees (employee_id, first_name, last_name)
+values (6, "jack", "reacher");
+       
+select * from employees;
+
+select first_name, last_name
+from employees;
+
+select last_name, first_name
+from employees;
+
+select * from employees
+where employee_id = 1;
+
+select *
+from employees
+where first_name = "jack";
+
+select *
+from employees
+where hourly_pay >= 15;
+
+select * from
+employees
+where hire_date <= "2023-03-15";
+
+select * from
+employees where employee_id != 2;
+
+use mydb;
+
+select * from employees;
+
+select * from 
+employees
+where hire_date is null;
+
+select * from 
+employees
+where hire_date is not null;
+
+update employees
+set hourly_pay = 10.50,
+hire_date = "2023-01-07"
+where employee_id = 6;
+
+update employees
+set hourly_pay = 10.50,
+hire_date = null
+where employee_id = 6;
+
+update employees
+set hourly_pay = 10.50;
+
+delete from employees
+where employee_id = 6;
+
+set autocommit = off;
+
+commit;
+
+delete from employees;
+
+rollback;
+
+select * from employees;
