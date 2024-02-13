57c8dea0ee2dfc36b87149725992a1eeb5a37d6c
Hamza Shafi <37909988+Hamzashafi@users.noreply.github.com>
2024-02-13 11:14:08 +0500

    File2.sql

--- Schema.sql
+++ Schema.sql
@@ -1,146 +1,124 @@
-create table test (
-	my_date date,
-    my_time time,
-    my_datetime datetime
-    );
-    
-insert into test
-    values (current_date(), current_time(), now()
-    );
-    
-insert into test 
-	values (current_date() + 1, null, null
-);
-    
-select * from test;
-
-drop table test;
-
-create table product (
-	product_id int,
-    product_name VARCHAR(25) UNIQUE,
-    price decimal(4, 2)
+create table customers (
+	customer_id int PRIMARY KEY auto_increment,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
 );
 
-alter table product
-	add constraint 
-		UNIQUE (product_name);
-        
-insert into product 
-VALUES
-	(101, "Pommes", 3.50),
-    (102, "Burger", 9.99),
-    (103, "Drink", 2.99);
-    
-    create table product (
-	product_id int,
-    product_name VARCHAR(25),
-    price decimal(4, 2) NOT NULL
-);
+insert into customers (first_name, last_name)
+	values 
+    ("jack", "black"),
+    ("panda", "kungfu"),
+    ("anabella", "norman");
 
-alter table product
-modify price DECIMAL (4, 2) NOT NULL;
+select * from customers;
 
-insert into product 
-values (104, "cookie", 0);
+drop table transactions;
 
-select * from product;
-
-create table employees (
-employee_id int,
-first_name VARCHAR (50),
-last_name VARCHAR (50),
-hourly_pay decimal (4, 2),
-hire_date date,
-	constraint chk_hourly_pay check (hourly_pay >= 10.00)
-);
-
-alter table employees 
-add constraint chk_hourly_pay check (hourly_pay >= 10.00);
-
-insert into employees
-values (6, "deadpool", "wolverine", 10.29, "2023-04-20");
-
-alter table employees
-drop check chk_hourly_pay;
-
-select * from employees;
-
-select * from product;
-
-insert into product 
-values (105,"straw", 0.00),
-		(106, "napkins", 0.00),
-		(107, "spoon", 0.00);
-        
-delete from product 
-where product_id >= 104;
-
-create table product (
-	product_id int,
-    product_name VARCHAR(25),
-    price decimal(4, 2) DEFAULT 0
-);
-
-alter table product
-	alter price set default 0;
-    
-insert into product (product_id, product_name)
-	values (104, "straw"),
-			(105, "spoon"),
-			(106, "napkins");
-
-select * from product;
-
-
-					-----------new lines----------------
-
-create table transactions(
-	transaction_id int,
+create table transactions (
+	transaction_id int PRIMARY KEY auto_increment,
     amount decimal (5, 2),
-    transaction_date datetime DEFAULT NOW()
+    customer_id int,
+    foreign key(customer_id) REFERENCES customers(customer_id)
 );
 
-insert into transactions (transaction_id, amount)
-	values (3, 9.99);
-
-drop table transactions;
-
-create table transactions (
-	transaction_id int PRIMARY KEY,
-    amount DECIMAL (5, 2)
-);
-
-alter table transactions
-add constraint 
-PRIMARY KEY(transaction_id);
-
-insert into transactions
-	values (null, 9.99);
-    
-select amount 
-	from transactions
-		where transaction_id = 0003;
-
-drop table transactions;
-
-create table transactions (
-	transaction_id int PRIMARY KEY AUTO_INCREMENT,
-    amount decimal (5, 2)
-);
-
-insert into transactions (amount)
-	values (2.99);
-
-delete from transactions 
-	where transaction_id = 4;
+alter table transactions 
+	drop foreign key transactions_ibfk_1;
     
 alter table transactions
-	auto_increment = 1000;
-
+	add constraint fk_customer_id
+		foreign key (customer_id) REFERENCES customers (customer_id); 
+        
 delete from transactions;
 
-insert into transactions (amount)
-	values (5.99);
+select * from transactions;
+
+alter table transactions
+	auto_increment = 1000;
+    
+insert into transactions (amount, customer_id)
+	values (4.99, 3),
+    (2.89, 2),
+    (3.38, 3),
+    (4.99, 1);
+    
+delete from customers 
+	where customer_id = 3;
+    
+use mydb;
+
+insert into transactions (amount, customer_id)
+	values (1.00, null);
+    
+insert into customers (first_name, last_name)
+	values("poopy", "puff");
 
 select * from transactions;
+
+select * from product;
+
+select * from customers;
+
+select transaction_id, amount, first_name, last_name
+ from transactions
+	inner join customers on 
+		transactions.customer_id = customers.customer_id;
+        
+select * from
+	transactions LEFT JOIN
+		customers on
+			transactions.customer_id = customers.customer_id;
+            
+select * from 
+	transactions RIGHT JOIN 
+		customers on 
+			transactions.customer_id = customers.customer_id;
+            
+select COUNT(amount) as count
+	from transactions;
+
+select MAX(amount) as maximum
+	from transactions;
+    
+select MIN(amount) as minimum
+	from transactions;
+    
+select AVG(amount) as average
+	from transactions;
+
+select SUM(amount) as sum
+	from transactions;
+    
+select * from employees;
+
+select CONCAT(first_name, " ", last_name) AS full_name
+	from employees;
+    
+
+alter table employees
+	add column job varchar(25) after hourly_pay;
+    
+update employees
+	set job = "janitor"
+		where employee_id = 6;
+        
+
+select * from
+	employees where
+		hire_date < "2023-01-5" AND job = "cook";
+        
+select * from employees
+	where job = "cook" OR job = "cashier";
+    
+select * from
+	employees where 
+		NOT job = "Manager" AND NOT job = "engineer";
+        
+select * from employees;
+
+
+select * from
+	employees where hire_date BETWEEN "2023-03-15" AND "2024-02-02";
+    
+    
+select * from employees
+	where job IN ("cashier", "cook", "janitor");
