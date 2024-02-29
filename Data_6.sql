912286e53d7f9d1360e7e2ea13164af566698bbd
Hamza Shafi <37909988+Hamzashafi@users.noreply.github.com>
2024-02-29 15:43:27 +0500

    Commit new lines

--- Data_6.sql
+++ Data_6.sql
@@ -410,3 +410,90 @@ SELECT item, customer_id, amount,
 SELECT first_name, country, age,
 	(SELECT SUM(age) FROM CUSTOMERS) AS Age_Count
 FROM CUSTOMERS;
+
+
+		-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+
+-- STORED PROCEDURE
+
+CREATE TABLE cars (
+    make varchar(100),
+    model varchar(100),
+    year int,
+    value decimal(10, 2)
+);
+
+INSERT INTO cars
+VALUES
+('Porsche', '911 GT3', 2020, 169700),
+('Porsche', 'Cayman GT4', 2018, 118000),
+('Porsche', 'Panamera', 2022, 113200),
+('Porsche', 'Macan', 2019, 27400),
+('Porsche', '718 Boxster', 2017, 48880),
+('Ferrari', '488 GTB', 2015, 254750),
+('Ferrari', 'F8 Tributo', 2019, 375000),
+('Ferrari', 'SF90 Stradale', 2020, 627000),
+('Ferrari', '812 Superfast', 2017, 335300),
+('Ferrari', 'GTC4Lusso', 2016, 268000);
+
+SELECT * FROM cars ORDER BY make, value DESC;
+
+DELIMITER //
+CREATE PROCEDURE get_all_cars()
+BEGIN
+    SELECT * FROM cars ORDER BY make, value DESC;
+END //
+DELIMITER ;
+
+-- Check permissions for user
+show grants for root@localhost;
+
+CALL get_all_cars;
+
+
+-- Procedure with input parameter
+DELIMITER //
+CREATE PROCEDURE get_cars_by_year(
+    IN year_filter INT
+)
+BEGIN
+    SELECT * FROM cars WHERE `year` = year_filter ORDER BY make, value DESC;
+END //
+DELIMITER ;
+
+CALL get_cars_by_year(2018);
+
+
+SELECT model, make, value,
+	(SELECT MAX(value)FROM CARS) AS maximum_value
+		FROM cars;
+
+SELECT 23 * 6 AS multiplication_result;
+
+-- SELECT 23 mod 6
+
+SELECT 40/564
+
+-- removes decimals placed from a number
+SELECT FLOOR(23/6) AS floor_result 
+
+-- Generate random number
+SELECT RAND() as random_number 
+
+
+SELECT * FROM information_schema.CHARACTER_SETS order BY CHARACTER_SET_NAME 
+
+
+-- Extract a domain from an email address
+SELECT SUBSTRING_INDEX("hamzahsafi946@gmail.com", "@", -1) 
+
+SELECT * from CARS
+
+EXPLAIN SELECT COUNT(*) 
+FROM COLLEGES 
+group by COLLEGE_CODE 
+
+
+SELECT COUNT(DISTINCT VALUE)  from CARS 
+
+show indexes from COLLEGES
